8901f91653b317a03730d8b5a012120ab24a412d
Drew Price <drewprice@users.noreply.github.com>
2015-09-09 15:30:38 -0400

    I didn't bake anything

--- spec/chocolate-babka-spec.js
+++ spec/chocolate-babka-spec.js
@@ -1,31 +1,37 @@
-describe('Chocolate Babka Counter', function() {
-  describe('basic counter closure accepting a starting number', function() {  
-    var bakeryCounter = counterCreator(0);
+describe('Chocolate Babka Count', function() {
+
+  describe('basic counter closure accepting a starting number', function() {
+
+    var bakeryCount = Count(0);
+
     it('should increment the number', function() {
-      expect(bakeryCounter()).toEqual(1);
+      expect(bakeryCount()).toEqual(1);
     });
+
     it('should keep track of the count', function() {
-      expect(bakeryCounter()).toEqual(2);
-      expect(bakeryCounter()).toEqual(3);
+      expect(bakeryCount()).toEqual(2);
+      expect(bakeryCount()).toEqual(3);
     });
   });
-  describe('Has a seperate bakery counter and babka counter', function() {
-    it('should increment only one counter', function() {
-      var bakeryCounter = counterCreator(3),
-      babkaCounter = counterCreator(5);
 
-      expect(bakeryCounter()).toEqual(4);
-      expect(babkaCounter()).toEqual(6);
+  describe('Has a seperate bakery counter and babka counter', function() {
+
+    it('should increment only one counter', function() {
+      var bakeryCount = Count(3),
+      babkaCount = Count(5);
+
+      expect(bakeryCount()).toEqual(4);
+      expect(babkaCount()).toEqual(6);
     });
   });
 });
 
 describe('Line number announcer', function() {
   describe('Seperate Announcers for Babkas and Baked Goods', function() {
-    var bakeryCounter = counterCreator(3),
-        babkaCounter = counterCreator(5),
-        bakeryAnnouncer = countAnnouncer('Baked Goods', bakeryCounter)
-        babkaAnnouncer = countAnnouncer('Babka', babkaCounter);
+    var bakeryCount = Count(3),
+        babkaCount = Count(5),
+        bakeryAnnouncer = Announce('Baked Goods', bakeryCount)
+        babkaAnnouncer = Announce('Babka', babkaCount);
 
     it('should announce who is now being served in both lines', function() {
       expect(bakeryAnnouncer()).toEqual('Now serving Baked Goods number 4!');
